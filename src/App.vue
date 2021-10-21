f6ee17b99d34c5386c461ef4902df234e6de39f2
Gen <1121567132@qq.com>
2021-10-21 21:00:29 +0800

    playerList是Map

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,10 @@
       <div @click="stopPlay">
         <img src="@/assets/images/stop.svg" />
       </div>
-
+                                      <!-- 可重叠则被选中状态的样式 -->
+      <div @click="changeOverlap" :class="{ selected: overlap }">
+        <img src="@/assets/images/over.svg" />
+      </div>
     </Control>
 
 
@@ -52,7 +55,8 @@ export default {
       // nowPlay是一个对象，存放正在播放的voiceItem
       nowPlay:{},
       // 放弃了，加一个播放列表存储每个正在播放的音频 播放完的清除
-      playerList: new Map()
+      playerList: new Map(),
+      overlap:false
     }
   },
   components: {
@@ -62,14 +66,35 @@ export default {
   },
   methods: {
     // 默认是可重叠播放的
+
+    // 不重叠播放：在播放下一个前把前一个暂停掉
+    // 要暂停掉上一个就需要找到上一个，在没有playerlist或者audio标签的情况下是找不到上一个的
+    // 创建playerlist后 从playerlist找到上一个音频 然后暂停
       play(voice) {
+          // 设定不同情况下的key 不能重叠时，都设定为once，而从保证播放列表里只有一条音频
+          // 在外部设置key变量
+          let key = ''
+          if(!this.overlap){
+            // 第一次播放，playerlist里还没有值
+            // 当有once这个key后暂停它
+            // 但不是第一次的情况下里面有once值就停止播放
+            // 但是之前还可能有key是时间戳的情况
+            // 此时之前在播放列表里的音声还会播放
+            // 需要取消掉
+            this.stopPlay()
+            key = 'once'
+          }else{
+            // 重复播放的情况不用多做处理，默认就是重复播放状态
+            // 这里传入key是为了特定每个audio 好在以后同时暂停
+            key = new Date().getTime()
+          }
         // todo ！！！播放逻辑
           // path 得到video路径
           // 放到里面可以在每次播放的时候灵活cdn和本地？ 为什么用const？
           // const voice = VoiceList.voices[0].voiceList[0]
 
           // 1.创建audio并随事件放入播放列表 以后好暂停
-          const key = new Date().getTime()
+          // const key = new Date().getTime()
           const path = `${CDN}/${voice.path}`
           let audio = new Audio(path)
           // key直接对应一个audio 不用.audio
@@ -79,8 +104,10 @@ export default {
           // 3.展示区
           this.nowPlay = voice
           // 4.播放结束后的善后
+          // 播放时就约定好 播放结束后清空展示区 离开播放列表
           audio.onended = () => {
-            this.nowPlay = 'null'
+            this.nowPlay = {}
+            this.playerList.delete(key)
           }
 
 
@@ -96,13 +123,16 @@ export default {
 
         // 1.暂停所有key对应的audio
         for (const key of this.playerList.keys()) {
+          // todo 因为只是暂停 没有播放结束 所以没有触发结束事件出列表嘛？
           this.playerList.get(key).pause()
         }
 
-        // 2.清空展示区
+        // 2.清空展示区和播放列表
         this.nowPlay = 'null'
-
-
+        this.playerList.clear()
+      },
+      changeOverlap(){
+        this.overlap = !this.overlap
       }
 
 
